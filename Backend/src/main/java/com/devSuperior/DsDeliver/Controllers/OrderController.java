f8cdae9d10783ca29003793b045158c70c8815bb
zNexTage <christianoliveira1908@hotmail.com>
2021-01-04 16:58:29 -0300

    POST /orders

--- Backend/src/main/java/com/devSuperior/DsDeliver/Controllers/OrderController.java
+++ Backend/src/main/java/com/devSuperior/DsDeliver/Controllers/OrderController.java
@@ -1,18 +1,19 @@
 package com.devSuperior.DsDeliver.Controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.jaxb.SpringDataJaxb.OrderDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devSuperior.DsDeliver.Dto.OrderDTO;
-import com.devSuperior.DsDeliver.Dto.ProductDTO;
 import com.devSuperior.DsDeliver.Services.OrderService;
-import com.devSuperior.DsDeliver.Services.ProductService;
 
 @RestController
 @RequestMapping(value = "/orders")
@@ -25,5 +26,14 @@ public class OrderController {
 		List<OrderDTO> list = service.findAll();
 		
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+				
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
--- Backend/src/main/java/com/devSuperior/DsDeliver/Services/OrderService.java
+++ Backend/src/main/java/com/devSuperior/DsDeliver/Services/OrderService.java
@@ -1,5 +1,6 @@
 package com.devSuperior.DsDeliver.Services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -9,14 +10,21 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devSuperior.DsDeliver.Dto.OrderDTO;
+import com.devSuperior.DsDeliver.Dto.ProductDTO;
 import com.devSuperior.DsDeliver.Entities.Order;
-import com.devSuperior.DsDeliver.Repositories.OrderRepository; 
+import com.devSuperior.DsDeliver.Entities.OrderStatus;
+import com.devSuperior.DsDeliver.Entities.Product;
+import com.devSuperior.DsDeliver.Repositories.OrderRepository;
+import com.devSuperior.DsDeliver.Repositories.ProductRepository; 
 
 @Service
 public class OrderService {
 	
 	@Autowired
 	private OrderRepository repository; 
+	
+	@Autowired
+	private ProductRepository prodRepository; 
 	 
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll(){
@@ -25,5 +33,21 @@ public class OrderService {
 		Stream<OrderDTO> strOrderDto = list.stream().map(ord -> new OrderDTO(ord));
 		
 		return strOrderDto.collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO orderDto){ 
+		Order order = new Order(0, orderDto.getAddress(), orderDto.getLatitude(), orderDto.getLongitude(), 
+				Instant.now(), OrderStatus.PENDING);
+		
+		//Faz o relacionamento entre a ordem e os produtos		
+		for(ProductDTO prodDto : orderDto.getProducts()) {
+			Product prod = prodRepository.getOne(prodDto.getId());
+			order.getProducts().add(prod);
+		}
+		
+		order = repository.save(order);
+		
+		return new OrderDTO(order);
 	}
 }
