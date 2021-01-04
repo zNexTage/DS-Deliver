816e42432fd0fd35ae532024e369b6558a9151c9
zNexTage <christianoliveira1908@hotmail.com>
2021-01-04 14:13:25 -0300

    Project Created

---
+++ Backend/src/main/java/com/devSuperior/DsDeliver/DsDeliverApplication.java
@@ -0,0 +1,13 @@
+package com.devSuperior.DsDeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsDeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsDeliverApplication.class, args);
+	}
+
+}
