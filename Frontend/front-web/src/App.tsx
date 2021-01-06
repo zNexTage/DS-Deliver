f9ba11a580414f36866f15d055fd6d7d536cc2fe
zNexTage <christianoliveira1908@hotmail.com>
2021-01-06 14:07:07 -0300

    Add Home Page

--- Frontend/front-web/src/App.tsx
+++ Frontend/front-web/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import './App.css';  
+import './App.css';
 import Navbar from './Components/Navbar/index';
+import Home from './Pages/Home/index'
 
 function App(): JSX.Element {
   return (
-    <div> 
-     <Navbar />
-    </div>
+    <>
+      <Navbar />
+      <Home />
+    </>
   );
 }
 
