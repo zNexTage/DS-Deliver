fd7702411f81843bff540af24423ae2f04edc3ce
zNexTage <christianoliveira1908@hotmail.com>
2021-01-06 13:18:30 -0300

    Create front-end initial files

---
+++ Frontend/front-web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';  
+
+function App(): JSX.Element {
+  return (
+    <div> 
+      <h1>
+        Hello
+      </h1>
+    </div>
+  );
+}
+
+export default App;
